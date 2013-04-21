f6a04b02cce02fe558d9226b540f13dbc5b66a41
Johnny Graber <dev@jgraber.ch>
2013-04-21 16:22:43 +0200

    start with plain new rails app

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rails 4 Demo: Books
+
+## Start
+`rails new books`
+
+`git init`
+
+`git add .`
+
+`git commit -m "start with plain new rails app"`
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Books::Application.config.secret_key_base = '6ae7c36ef286cdb2ba03ba62efcc8f2528db613a4bdd869b4020267a46d67f5a111255b19d616c0f2eedded3ea9665e4666d70e57e734c5a76d198443ad2a9d5'
